7bf1ab904c943d62d3db9af907e430349e2b80ec
Sahar Banisafar <banisafar.sahar@gmail.com>
2017-04-30 13:17:01 -0400

    Add files via upload

--- byte_data_challenge.py
+++ byte_data_challenge.py
@@ -1,122 +1,97 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+#from scipy.stats.stats import pearsonr
+import statsmodels.api as sm
+from statsmodels.iolib.summary2 import summary_col
 
-##########first split data, now commit it out######
-#data= pd.read_csv('kc_house_train_data.csv')
-#train_house, test_house = train_test_split(data, test_size = 0.2)  #split up data
-#train_house.to_csv('train_house.csv')
-#test_house.to_csv('test_house.csv')
+np.random.seed(1)  # to get the same split everytime
 
 
-data= pd.read_csv('train_house.csv')
-test_data= pd.read_csv('test_house.csv')
+all_data= pd.read_csv('kc_house_train_data.csv')
+data, test_data = train_test_split(all_data, test_size = 0.2)  #split up data
 
-#normalize(?)
+#################### definitions ################
 
-single_features= list(data.columns.values)
-single_features.remove('Unnamed: 0')
-single_features.remove('id')
-single_features.remove('date')
-single_features.remove('price')
-print single_features
+def make_a_feature_list(data,feat):
+    X = data[feat]
+    Y = sm.add_constant(X)  # add a constant
+    return Y
 
-#for feature in single_features:
-#    print feature
+def make_a_price_list(data,feat):
+    return data[feat]
 
-small_features='sqft_lot'
-medium_features=['sqft_living','bedrooms','zipcode']
+def solve_MSE(test,Y):
+    return np.mean(((test - Y))**2)
+
+def OLS_fit_predict(Y,X,X_test):   
+    results = sm.OLS(Y, X).fit()
+    y_pred = results.predict(X_test)
+    return y_pred
+
+#################### FIRST PART ##########################################
+all_features= list(data.columns.values)
+all_features.remove('id')  # irrelevant features - remove
+all_features.remove('date')
+all_features.remove('price')
 
 
 prices=data['price']  # target value
-small_data=data[small_features]
-medium_data=data[medium_features]
-large_data=data
-del large_data['price']  #delete irrelevant columns
-del large_data['id']
-del large_data['date']
-
-
-#Test data and prices
 test_price=test_data['price']
-small_test=test_data[small_features]
-medium_test=test_data[medium_features]
-large_test=test_data
-del large_test['price']  #delete irrelevant columns
-del large_test['id']
-del large_test['date']
 
 
+table=pd.DataFrame(index=all_features, columns=['MSE']) # empty dataframe
+
+for feature in all_features:
+    single_feature = make_a_feature_list(data,feature)
+    single_feature_test=make_a_feature_list(test_data,feature)
+    y_pred = OLS_fit_predict(prices,single_feature,single_feature_test)
+    MSEE= solve_MSE(test_price,y_pred)
+    table.set_value(feature,'MSE', MSEE)
 
 
+print table[table['MSE'] == min(table['MSE'])] # this is the min MSE
+print table[table['MSE'] == max(table['MSE'])]  # this is the max MSE
 
+# we see that longitude has the smalleset MSE - a better analysis would be to look at correlation.  
+#can see multiple analysis when look at summary 
 
+###################### SECOND PART #######################################
+small_features = 'sqft_living'
+medium_features = ['sqft_living','bedrooms','bathrooms']
+large_features = all_features  # id/date/price removed
 
+small_data = make_a_feature_list(data,small_features)  # data
+medium_data = make_a_feature_list(data,medium_features)
+large_data = make_a_feature_list(data,large_features)
 
-
-
-
-###if want to see features individually
-##for feature in single_features:
-##    print feature
-##    small_data=data[feature]
-##    linear = LinearRegression()
-##    linear.fit(small_data.to_frame(), prices.to_frame())  
-##    print linear.coef_
-##    X=linear.predict(small_test.to_frame())  # this is a numpy.ndarray
-##    Y=pd.DataFrame(X)
-##    print np.mean(((test_price - Y)[0])**2)
-
-
-
-
+small_test = make_a_feature_list(test_data,small_features)  # test data
+medium_test = make_a_feature_list(test_data,medium_features)
+large_test = make_a_feature_list(test_data,large_features)
 
 #Linear Regression for 1 feature
-linear = LinearRegression()
-linear.fit(small_data.to_frame(), prices.to_frame())  
-print linear.coef_
-X=linear.predict(small_test.to_frame())  # this is a numpy.ndarray
-Y=pd.DataFrame(X)
-print np.mean(((test_price - Y)[0])**2)  # this is producing a 3477*3477 - y?
-
-
-
-#improve visualizations
-plt.scatter(small_test, test_price,  color='black')
-plt.plot(small_test, linear.predict(small_test.to_frame()), color='blue',
-         linewidth=3)
-plt.xticks(())
-plt.yticks(())
-plt.show()
- 
-
-
+y_pred = OLS_fit_predict(prices,small_data,small_test)
+print(len(y_pred))
+MSEE= solve_MSE(test_price,y_pred)
+print MSEE
 
 #Linear Regression for 3 chosen features
-linear = LinearRegression()
-
-linear.fit(medium_data, prices.to_frame())  
-print linear.coef_
-X=linear.predict(medium_test)  # this is a numpy.ndarray
-Y=pd.DataFrame(X)
-print np.mean(((test_price - Y)[0])**2)
-
-
+y_pred = OLS_fit_predict(prices,medium_data,medium_test)
+MSEE= solve_MSE(test_price,y_pred)
+print MSEE
 
 #Linear Regression for all 19 features
-linear = LinearRegression()
+y_pred = OLS_fit_predict(prices,large_data,large_test)
+MSEE= solve_MSE(test_price,y_pred)
+print MSEE
 
-linear.fit(large_data, prices.to_frame())  
-print linear.coef_
-X=linear.predict(large_test)  # this is a numpy.ndarray
-Y=pd.DataFrame(X)
-print np.mean(((test_price - Y)[0])**2)
-
-
-#SECOND PART
-
+#######################  THIRD PART #####################################
+zipcode_data = data.groupby('zipcode').aggregate(np.mean) # averages the means of all
+print zipcode_data[zipcode_data['price'] == max(zipcode_data['price'])]['price'] # see that certain areas are pricer than others
+print zipcode_data[zipcode_data['price'] == min(zipcode_data['price'])]['price']
+print zipcode_data[zipcode_data['price'] > 1000000]['price']
 
 
 
